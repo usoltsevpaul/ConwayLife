02978d12295dc68da43d39ad248c28f93cf88c4a
usoltsevpaul <paulusoltsev@gmail.com>
2017-05-03 14:26:11 -0400

    intermediate check in, saving changes for later

--- life.js
+++ life.js
@@ -1,12 +1,51 @@
+var board = [[]]; // 2d matrix
+var text = "";
+
 $(function() {
-    var text = "";
-    for (var i = 0; i < 4; i++) {
+    for (var y = 10; y >= 0; y--) { // height - row - y
+        var x = 0;
+        board[y] = []
         text += "<tr>";
-        for (var j = 0; j < 4; j++) {
-            text += "<td><img class=\"octagon\" src=\"assets/Mesh-100.png\" /></td>";
+        for (x; x <= 20; x++) { // width - column - x
+            if (Math.floor(Math.random() * 5) >= 4) {
+                board[y][x] = new Life(x, y);
+                text += "<td id=\"" + y + ":" + x + "\"><img class=\"octagon\" src=\"assets/Mesh-100.png\" /></td>";
+            } else {
+                board[y][x] = undefined;
+                text += "<td id=\"" + y + ":" + x + "\"></td>"
+            }
         }
-        text += "<\tr>";
+        text += "</tr>";
     }
-    console.log(text);
+
+    console.log(board);
     document.getElementById("table").innerHTML = text;
+
+    setInterval(tick, 4000);
 })
+
+function tick() {
+    board.forEach(function(row) {
+        row.forEach(function(life) {
+            if (life) {
+                var neighbours = 0;
+                if (board[(life.y - 1) % 20][(life.x + 1) % 10]) {
+                    neighbours++;
+                }
+
+                if (neighbours > 0) {
+                    document.getElementById(life.x + ":" + life.y).innerHTML = "";
+                    board[life.x][life.y] = undefined;
+                }
+            }
+        }, this);
+    }, this);
+}
+
+class Life {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.dead = false;
+    }
+}
