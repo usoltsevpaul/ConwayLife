4a97845fe61b6d54d035d115f22af3b1f4efb8d0
usoltsevpaul <paulusoltsev@gmail.com>
2017-05-04 18:05:07 -0400

    works now, need to clean up

--- life.js
+++ life.js
@@ -1,31 +1,31 @@
 var board = [[]]; // 2d matrix
 var text = "";
+var width = 40;
+var height = 375;
+var interval = 100;
 
 $(function() {
-    for (var y = 10; y >= 0; y--) { // height - row - y
-        var x = 0;
-        board[y] = []
+    for (var x = 0; x < width; x++) {
+        var y = 0;
+        board[x] = []
         text += "<tr>";
-        for (x; x <= 20; x++) { // width - column - x
+        for (y; y < height; y++) {
             if (Math.floor(Math.random() * 5) >= 4) {
-                board[y][x] = new Life(x, y);
-                text += "<td id=\"" + y + ":" + x + "\"><img class=\"octagon\" src=\"assets/Mesh-100.png\" /></td>";
+                board[x][y] = new Life(x, y, "alive");
+                text += "<td id=\"" + x + "," + y + "\"><img class=\"octagon\" src=\"assets/Mesh-100.png\" /></td>";
             } else {
-                board[y][x] = undefined;
-                text += "<td id=\"" + y + ":" + x + "\"></td>"
+                board[x][y] = new Life(x, y, "dead");
+                text += "<td id=\"" + x + "," + y + "\"></td>"
             }
         }
         text += "</tr>";
     }
 
-    console.log(board);
     document.getElementById("table").innerHTML = text;
-
-    setInterval(tick, 4000);
-
+    setInterval(tick, interval);
     setTimeout(function () {
-        setInterval(cleanup, 4000);
-    }, 2000)
+        setInterval(cleanup, interval);
+    }, interval / 2)
 })
 
 function tick() {
@@ -33,13 +33,35 @@ function tick() {
         row.forEach(function(life) {
             if (life) {
                 var neighbours = 0;
-                if (board[(life.y) % 20][(life.x) % 10]) {
+                
+                if (board[(life.x - 1 + width) % width][(life.y + 1) % height].state == "alive") { // 1
+                    neighbours++;
+                }
+                if (board[(life.x) % width][(life.y + 1) % height].state == "alive") { // 2
+                    neighbours++;
+                }
+                if (board[(life.x + 1) % width][(life.y + 1) % height].state == "alive") { // 3
+                    neighbours++;
+                }
+                if (board[(life.x - 1 + width) % width][(life.y) % height].state == "alive") { // 4
+                    neighbours++;
+                }
+                if (board[(life.x + 1) % width][(life.y) % height].state == "alive") { // 5
+                    neighbours++;
+                }
+                if (board[(life.x - 1 + width) % width][(life.y - 1 + height) % height].state == "alive") { // 6
+                    neighbours++;
+                }
+                if (board[(life.x) % width][(life.y - 1 + height) % height].state == "alive") { // 7
+                    neighbours++;
+                }
+                if (board[(life.x + 1) % width][(life.y - 1 + height) % height].state == "alive") { // 8
                     neighbours++;
                 }
 
-                if (Math.floor(Math.random() * 5) >= 3) {
+                if (life.state == "alive" && (neighbours < 2 || neighbours > 3)) {
                     life.state = "dying";
-                } else {
+                } else if (life.state == "dead" && neighbours == 3) {
                     life.state = "born";
                 }
             }
@@ -52,10 +74,10 @@ function cleanup() {
         row.forEach(function(life) {
             if (life) {
                 if (life.state == "dying") {
-                    document.getElementById(life.y + ":" + life.x).innerHTML = "";
+                    document.getElementById(life.x + "," + life.y).innerHTML = "";
                     life.state = "dead";
                 } else if (life.state == "born") {
-                    document.getElementById(life.y + ":" + life.x).innerHTML = "<img class=\"octagon\" src=\"assets/Mesh-100.png\" />";
+                    document.getElementById(life.x + "," + life.y).innerHTML = "<img class=\"octagon\" src=\"assets/Mesh-100.png\" />";
                     life.state = "alive";
                 }
             }
@@ -64,9 +86,9 @@ function cleanup() {
 }
 
 class Life {
-    constructor(x, y) {
+    constructor(x, y, state) {
         this.x = x;
         this.y = y;
-        this.state = "dead"; // "born", "alive", "dying", "dead"
+        this.state = state; // "born", "alive", "dying", "dead"
     }
 }
